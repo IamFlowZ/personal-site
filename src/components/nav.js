04a041119cd84cbcbb390728f7e9d6f2c28b52f7
IamFlowZ <dakota.lewallen17@gmail.com>
2020-01-21 19:32:27 -0500

    hiding the nav bar on down scrolls

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,12 +7,13 @@ const scrollFunc = () => {
 
 export default function() {
     const [scrollY, setScrollY] = useState(0)
-    const prevPos = window.pageYOffset
+    const [show, setShow] = useState(true)
     useEffect(_ => {
         const handleScroll = _ => {
-            if(window.pageYOffset > 1) {
-                console.log('scrolled', window.pageYOffset)
-            }
+            (window.pageYOffset > scrollY) ?
+                setShow(false):
+                setShow(true)
+            setScrollY(window.pageYOffset)
         }
         window.addEventListener('scroll', handleScroll)
         return _ => {
@@ -20,7 +21,7 @@ export default function() {
         }
     })
     return (
-        <div id="navbar" onScroll={scrollFunc}>
+        <div id="navbar" onScroll={scrollFunc} style={{top: show ? "0": "-50px"}}>
             <a href="#home">Home</a>
             <a href="#news">News</a>
             <a href="#contact">Contact</a>
