901ec72b0434270505f673d38b8425a9dab45a57
IamFlowZ <dakota.lewallen17@gmail.com>
2020-02-02 17:44:38 -0500

    added react router, moved nav comp into root of app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-import Nav from './components/nav/nav'
-import Post from './components/post/post'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom'
 
-  
+import './components/nav/nav.css'
+import Post from './components/post/post'
+import Contact from './components/contact/contact'
+
+const scrollFunc = () => {
+  console.log('i scrolled')
+}
+
 function App() {
+  const [scrollY, setScrollY] = useState(0)
+  const [show, setShow] = useState(true)
+  useEffect(_ => {
+      const handleScroll = _ => {
+          (window.pageYOffset > scrollY) ?
+              setShow(false):
+              setShow(true)
+          setScrollY(window.pageYOffset)
+      }
+      window.addEventListener('scroll', handleScroll)
+      return _ => {
+          window.removeEventListener('scroll', handleScroll)
+      }
+  })
   return (
     <div className="App" >
-      <Nav />
-      <Post />
+      <Router>
+          <ul id="navbar" style={{listStyleType:"none", top: show ? "0": "-50px"}} onScroll={scrollFunc}>
+              <li className="navItem">
+                  <Link to="/">Home</Link>
+              </li>
+              <li className="navItem">
+                  <Link to="/">Posts</Link>
+              </li>
+              <li className="navItem">
+                  <Link to="/contact">Contact</Link>
+              </li>
+              <input type="text"></input>
+              <li className="navItem">
+                  <Link to="/">Search</Link>
+              </li>
+          </ul>
+          <Switch>
+              <Route path="/contact">
+                <Contact/>
+              </Route>
+              <Route path="/">
+                <Post />
+              </Route>
+          </Switch>
+      </Router>
     </div>
   );
 }
