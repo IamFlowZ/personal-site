f9488d6b2585adcdee10e12a16ede648e1993f62
IamFlowZ <dakota.lewallen17@gmail.com>
2020-02-08 11:52:51 -0500

    starting contact page

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import './contact.css'
+import profile from '../../assets/personal/me.png'
+import linkedIn from '../../assets/social/linkedIn.svg'
+import twitter from '../../assets/social/twitter.svg'
+import github from '../../assets/social/github.svg'
 
 export default function() {
     return (
-        <div>Contact</div>
+        <div className="contactContainer">
+            <div>
+                <span></span>
+                <img src={profile} alt="my likeness"></img>
+                <div className="contactSocial">
+                    <p>Find me on: </p>
+                    <img src={linkedIn} alt="linked in logo" style={{margin: 0, height: "2em", width: "2em", marginRight: "1em"}}></img>
+                    <img src={github} alt="github logo" style={{margin: 0, height: "2em", width: "2em", marginRight: "1em"}}></img>
+                    <img src={twitter} alt="twitter logo" style={{margin: 0, height: "2em", width: "2em"}}></img>
+                </div>
+            </div>
+            <div className="bio">
+                <h2>Dakota Lewallen</h2>
+                <p>Software Developer</p>
+                <p>Bio</p>
+            </div>
+        </div>
     )
 }
