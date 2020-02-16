7eea4c0ab11560cb9481cf78976a4a65d673a4c5
IamFlowZ <dakota.lewallen17@gmail.com>
2020-02-16 18:04:07 -0500

    added font awesome for arrows

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,6 +1,10 @@
 import React, {useState} from "react";
 import ReactMarkdown from 'react-markdown'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons'
+import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons'
+
 import '@brainhubeu/react-carousel/lib/style.css';
 import Carousel from '@brainhubeu/react-carousel'
 
@@ -19,9 +23,11 @@ export default function({id, children}) {
     const [state, changeState] = useState(0)
     const onChange = (event) => changeState(event.target)
     return (
-        <div id={id} style={{marginBottom:"30em", width: '70%', marginLeft:"15%"}}>
+        <div id={id} style={{marginBottom:"20em", width: '70%', marginLeft:"15%"}}>
             <Carousel 
-                arrows={true} 
+                arrowLeft={<FontAwesomeIcon className="arrows" icon={faArrowCircleLeft} size="3x"/>}
+                arrowRight={<FontAwesomeIcon className="arrows" icon={faArrowCircleRight} size="3x"/>}
+                addArrowClickHandler={true}
                 value={state} 
                 onChange={onChange}
                 autoPlay={3000}
