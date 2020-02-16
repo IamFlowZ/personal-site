8ad13e68871bb12e85751e9a1be2ec6b7ad329ca
IamFlowZ <dakota.lewallen17@gmail.com>
2020-02-16 15:25:46 -0500

    changing up the color pallete

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -2,11 +2,13 @@ import React, {useState} from "react";
 import '@brainhubeu/react-carousel/lib/style.css';
 import Carousel, {Dots} from '@brainhubeu/react-carousel'
 
+import './projects.css'
+
 import aws from "../../assets/social/aws.svg"
 
 function SkillCarousel({src, i}) {
     return (
-        <div>
+        <div style={{backgroundColor:"black", color: "white"}}>
             <img src={src} alt="aws logo" style={{height: "250px", width:"550px", backgroundColor:"white"}}></img>
             <p>Thing {i}</p>
         </div>
@@ -17,8 +19,12 @@ export default function({id}) {
     const [state, changeState] = useState(0)
     const onChange = (event) => changeState(event.target)
     return (
-        <div id={id} style={{marginBottom:"37em"}}>
-            <Carousel arrows={true} value={state} onChange={onChange}>
+        <div id={id} style={{marginBottom:"30em", width: '70%', marginLeft:"15%"}}>
+            <Carousel 
+                arrows={true} 
+                value={state} 
+                onChange={onChange}
+            >
                 <SkillCarousel src={aws} i={1}></SkillCarousel>
                 <SkillCarousel src={aws} i={2}></SkillCarousel>
                 <SkillCarousel src={aws} i={3}></SkillCarousel>
