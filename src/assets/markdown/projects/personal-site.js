f9e31cc7835a7ba54c398432087de8b86f7d7313
IamFlowZ <dakota.lewallen17@gmail.com>
2020-02-22 20:11:45 -0500

    formatting

--- src/assets/markdown/projects/personal-site.js
+++ src/assets/markdown/projects/personal-site.js
@@ -1,2 +1,5 @@
-const text = `## [My Portfolio](https://github.com/IamFlowZ/personal-site)\n This site is built with [React](https://reactjs.org/) and the [create-react-app](https://github.com/facebook/create-react-app) boilerplate. Continuous Integration is done via [CircleCI](https://circleci.com/). And it's deployed using services from AWS ([S3](https://aws.amazon.com/s3/) for object storage and [CloudFront](https://aws.amazon.com/cloudfront/) for distrubition).`
+const text = `
+## [My Portfolio](https://github.com/IamFlowZ/personal-site)\n 
+This site is built with [React](https://reactjs.org/) and the [create-react-app](https://github.com/facebook/create-react-app) boilerplate. 
+Continuous Integration is done via [CircleCI](https://circleci.com/). And it's deployed using services from AWS ([S3](https://aws.amazon.com/s3/) for object storage and [CloudFront](https://aws.amazon.com/cloudfront/) for distrubition).`
 export default text
