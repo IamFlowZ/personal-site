f5938c3f1fff8aba708e979f0e728fe3120fbe9c
IamFlowZ <dakota.lewallen17@gmail.com>
2020-02-22 20:12:26 -0500

    starting on responsive carousels

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -22,23 +22,32 @@ export function CarouselItem({src, text}) {
     )
 }
 
-export default function({id, children}) {
+export default function({id, children, title}) {
     const [state, changeState] = useState(0)
     const onChange = (event) => changeState(event.target)
     return (
-        <div id={id} className="carousel" style={{marginBottom:"20em", width: '100%', position: 'relative'}}>
-            <Carousel 
-                arrowLeft={<FontAwesomeIcon className="arrows" icon={faArrowCircleLeft} size="3x"/>}
-                arrowRight={<FontAwesomeIcon className="arrows" icon={faArrowCircleRight} size="3x"/>}
-                addArrowClickHandler={true}
-                value={state} 
-                onChange={onChange}
-                autoPlay={4000}
-                stopAutoPlayOnHover={true}
-                infinite={true}
-                slides={children}>
-            </Carousel>
-        </div>
-        
+        <>
+            <h1 id={id} style={{marginBottom: '0.25em'}}>{title}</h1>
+            <div className="carousel" style={{marginBottom:"20em", width: '100%', position: 'relative'}}>
+                <Carousel 
+                    arrowLeft={<FontAwesomeIcon className="arrows" icon={faArrowCircleLeft} size="3x"/>}
+                    arrowRight={<FontAwesomeIcon className="arrows" icon={faArrowCircleRight} size="3x"/>}
+                    addArrowClickHandler={true}
+                    value={state} 
+                    onChange={onChange}
+                    autoPlay={4000}
+                    stopAutoPlayOnHover={true}
+                    infinite={true}
+                    slides={children}
+                    breakpoints={{
+                        1200: {
+                            arrowLeft: false,
+                            arrowRight: false
+                        }
+                    }}
+                    >
+                </Carousel>
+            </div>
+        </>
     )
 }
