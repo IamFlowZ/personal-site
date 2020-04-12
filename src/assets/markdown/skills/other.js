b6650b1e3f65c4a6e35788fd088575f0b33f6ec9
iamflowz <dakota.lewallen17@gmail.com>
2020-04-12 13:27:50 -0400

    other skills text

--- src/assets/markdown/skills/other.js
+++ src/assets/markdown/skills/other.js
@@ -1,4 +1,4 @@
-const text = `I have worked with various languages and database systems since starting my journey as a developer. 
-I have enjoyed learning and building with Golang and neo4j. 
-If you'd like to see what I've been working on, you'll have to scroll down *a tad bit more.*`
-export default text
+const text = `I'm a strong believer that you should use the right tool for the job. In software development, there's a lot of tools out there that provide all kinds of unique capabilities. 
+So, a large part of my developer journey has been learning how to tell what set of tools fit a bill for a project. 
+This has led me down a varied path, where I've found tools I adore... While others, not so much. In either case, I'm always open to exploring and can't wait to see what comes next.`;
+export default text;
