ccc195d462fbbc03851bc1ca27b10ab35c42b4bb
iamflowz <dakota.lewallen17@gmail.com>
2020-04-14 12:32:24 -0400

    updating my certification

--- src/assets/markdown/skills/neo4j.js
+++ src/assets/markdown/skills/neo4j.js
@@ -1,5 +1,5 @@
 const text = `
-I'm currently working on becoming a [certified Neo4j Developer](https://neo4j.com/graphacademy/neo4j-certification/) through [Neo4j's Graph Academy](https://neo4j.com/graphacademy/). 
+I recently became a [Certified Neo4j Professional](https://graphacademy.neo4j.com/certificates/362a1440003ee5e6f6aa98d7bae0849b8b7811ee58714d0ec83d598f3e82ef40.pdf) through [Neo4j's Graph Academy](https://neo4j.com/graphacademy/). 
 Neo4j is so easy to get started with, that I've been able to make working products without any additional training.
 However learning the technology from it's creators has provided me with more depth than what I could have achieved solely on my own.
 `;
